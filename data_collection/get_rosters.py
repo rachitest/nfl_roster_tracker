86142da32525a95af75713a884d633bced480303
rachitest <rachit-sabharwal@outlook.com>
2020-08-23 20:17:57 -0500

    get latest roster information

--- data_collection/get_rosters.py
+++ data_collection/get_rosters.py
@@ -1,4 +1,60 @@
 import pandas as pd
 
-from sportsreference.nfl.roster import Roster
+class Team:
+    def __init__(self, name, abbr, site):
+        self.name = name
+        self.abbr = abbr
+        self.site = site
+    
+    def get_roster(self):
+        url = self.site + "team/players-roster"
+        df = pd.read_html(url)
+        
+        active = df[0]
+        active_pup = df[1]
+        reserve_pup = df[2]
+        reserve_retired = df[3]
+        exempt = df[4]
+        
+        roster = {"Active":active,
+                "Active/PUP":active_pup,
+                "Reserve/PUP":reserve_pup,
+                "Reserve/Retired":reserve_retired,
+                "Exempt":exempt
+                }
+
+        return roster
+
+ARI = Team("Arizona Cardinals", "ARI", "https://www.azcardinals.com/")
+ATL = Team("Atlanta Falcons", "ATL", "https://www.atlantafalcons.com/")
+BAL = Team("Baltimore Ravens", "BAL", "https://www.baltimoreravens.com/")
+BUF = Team("Buffalo Bills", "BUF", "https://www.buffalobills.com/")
+CAR = Team("Carolina Panthers", "CAR" , "https://www.panthers.com/")
+CHI = Team("Chicago Bears", "CHI", "https://www.chicagobears.com/")
+CIN = Team("Cincinnati Bengals", "CIN", "https://www.bengals.com/")
+CLE = Team("Cleveland Browns", "CLE", "https://www.clevelandbrowns.com/")
+DAL = Team("Dallas Cowboys", "DAL", "https://www.dallascowboys.com/")
+DEN = Team("Denver Broncos", "DEN", "https://www.denverbroncos.com/")
+DET = Team("Detroit Lions", "DET", "https://www.detroitlions.com/")
+GB = Team("Green Bay Packers", "GB", "https://www.houstontexans.com/")
+HOU = Team("Houston Texans", "HOU", "https://www.packers.com/")
+IND = Team("Indianapolis Colts", "IND", "https://www.colts.com/")
+JAX = Team("Jacksonville Jaguars", "JAX", "https://www.jaguars.com/")
+KC = Team("Kansas City Chiefs", "KC", "https://www.chiefs.com/")
+LAC = Team("Los Angeles Chargers", "LAC", "https://www.chargers.com/")
+LAR = Team("Los Angeles Rams", "LAR", "https://www.therams.com/")
+LV = Team("Las Vegas Raiders", "LV", "https://www.raiders.com/")
+MIA = Team("Miami Dolphins", "MIA", "https://www.miamidolphins.com/")
+MIN = Team("Minnesota Vikings", "MIN", "https://www.vikings.com/")
+NE = Team("New England Patriots", "NE", "https://www.patriots.com/")
+NO = Team("New Orleans Saints", "NO", "https://www.neworleanssaints.com/")
+NYG = Team("New York Giants", "NYG", "https://www.giants.com/")
+NYJ = Team("New York Jets", "NYJ", "https://www.newyorkjets.com/")
+PHI = Team("Philadelphia Eagles", "PHI", "https://www.philadelphiaeagles.com/")
+PIT = Team("Pittsburgh Steelers", "PIT", "https://www.steelers.com/")
+SEA = Team("Seattle Seahawks", "SEA", "https://www.seahawks.com/")
+SF = Team("San Francisco 49ers", "SF", "https://www.49ers.com/")
+TB = Team("Tampa Bay Buccaneers", "TB", "https://www.buccaneers.com/")
+TEN = Team("Tennessee Titans", "TEN", "https://www.titansonline.com/")
+WAS = Team ("Washington Football Team", "WAS", "https://www.redskins.com/")
 
