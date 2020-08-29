e40360efccc89d499b7257306369e60a7b8a5d98
rachitest <rachit-sabharwal@outlook.com>
2020-08-28 21:16:53 -0500

    update Set-Location

--- data_collection/data_automation.ps1
+++ data_collection/data_automation.ps1
@@ -2,7 +2,7 @@ Set-Location "G:\My Drive\Backup\Documents\Python Projects\nfl_roster_tracker"
 
 poetry shell
 
-Set-Location "G:\My Drive\Backup\Documents\Python Projects\nfl_roster_tracker\data_collection"
+Set-Location "data_collection"
 
 python get_rosters.py
 
