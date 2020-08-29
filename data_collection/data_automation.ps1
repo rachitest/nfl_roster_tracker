03b5efbae8531d1b782de70f8f96a8dc73f48d46
rachitest <rachit-sabharwal@outlook.com>
2020-08-28 21:12:46 -0500

    create powershell script to automate data collection

---
+++ data_collection/data_automation.ps1
@@ -0,0 +1,7 @@
+cd "G:\My Drive\Backup\Documents\Python Projects\nfl_roster_tracker\data_collection"
+
+poetry shell
+
+python get_rosters.py
+
+git add NFL_rosters.db; git commit -m "Update NFL_rosters.db"; git push origin origin nfl_roster_tracker-master
