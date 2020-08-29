88df59fb5b1f52539dab6b348276c822d8229149
rachitest <rachit-sabharwal@outlook.com>
2020-08-28 21:40:42 -0500

    change poetry shell to poetry run

--- data_collection/data_automation.ps1
+++ data_collection/data_automation.ps1
@@ -1,7 +1,5 @@
 Set-Location "G:\My Drive\Backup\Documents\Python Projects\nfl_roster_tracker\data_collection"
 
-poetry shell
-
-python get_rosters.py
+poetry run python get_rosters.py
 
 git add NFL_rosters.db; git commit -m "Update NFL_rosters.db"; git push origin master
